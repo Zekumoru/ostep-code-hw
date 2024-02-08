4c20b398d02ac6a69919b132241eeb101764fba9
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-02-08 01:04:28 +0100

    Pass data from a child process to another child process using pipe()

---
+++ 01-fork/08-pipe.c
@@ -0,0 +1,65 @@
+/**
+ * Write a program that creates two children, and connects the
+ * standard output of one to the standard input of the other,
+ * using the pipe() system call.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+const char filename[] = "test.output";
+const int strSize = 100;
+
+void input(int in_pipe)
+{
+  char str[strSize];
+  read(in_pipe, str, strSize);
+
+  printf("Child %d has received message: %s\n", (int)getpid(), str);
+}
+
+void output(int out_pipe)
+{
+  char str[strSize];
+  sprintf(str, "This message is written by child process %d!", (int)getpid());
+  write(out_pipe, str, strSize);
+}
+
+int main()
+{
+  int pipefd[2]; // 0: read, 1: write
+  if (pipe(pipefd) == -1)
+  {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+
+  int inChild = fork();
+
+  if (inChild == 0)
+  {
+    int outChild = fork();
+
+    if (outChild == 0)
+    {
+      output(pipefd[1]);
+    }
+    else
+    {
+      wait(NULL);
+      input(pipefd[0]);
+    }
+  }
+  else
+  {
+    wait(NULL);
+    printf("Program terminated.\n");
+  }
+
+  return 0;
+}
